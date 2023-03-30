f5db26fc486ad97aa34989609cb1048b29d3e51a
Sylvain Chabert <sylvain.chabert@gmail.com>
2023-03-30 10:47:01 +0200

    Add component stub

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Error: Inline worker is not supported
 
 This error is thrown by the dependency [worker-loader](https://github.com/webpack-contrib/worker-loader/blob/22275e9cb0b67bc008ea2b008542303493eede18/src/runtime/inline.js#L46).
 
+I tried using the stub mechanism used by [vue/test-utils](https://test-utils.vuejs.org/api/#global), but the error still occurs.
+
+I found the fix used by @sbourouis for [jest](https://github.com/hrynko/vue-pdf-embed/issues/5) but did not found the same trick for vitest and vue/test-utils.
+
+Even when skipping the test, the error still occurs...
+
 ## Lead
 
 As the *work-loader* library is used in the deprecated version 3.0.0, would it be better to use an up to date version ?
--- src/components/__tests__/PdfViewer.spec.ts
+++ src/components/__tests__/PdfViewer.spec.ts
@@ -1,11 +1,19 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import PdfViewer from '../PdfViewer.vue'
 
 describe('PdfViewer', () => {
-  it('renders properly', () => {
-    const wrapper = mount(PdfViewer, {} );
-    expect(wrapper.text()).toContain('My Pdf');
-  })
+    it.skip('renders properly', () => {
+        const wrapper = mount(PdfViewer, {
+            global: {
+                stubs: {
+                    'vue-pdf-embed': {
+                        template: '<span>PDF VIEWER</span>',
+                    },
+                },
+            },
+        });
+        expect(wrapper.text()).toContain('My Pdf');
+    })
 })
